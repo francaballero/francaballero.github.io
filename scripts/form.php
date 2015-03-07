dda8805871e97b07c4417e9126851a76535e7817
caballerofran <fjcc.92@gmail.com>
2015-03-07 20:04:52 +0100

    Attached file, captcha, design

--- scripts/form.php
+++ scripts/form.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
-<html lang="">
+<html lang="en">
 
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="">
+	<meta name="description" content="../favicon.ico">
 	<meta name="author" content="">
-	<title>Fran Caballero - Personal page</title>
+	<title>Fran Caballero - Software developer</title>
 	<link rel="shortcut icon" href="">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="../styles/main.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
@@ -24,54 +25,98 @@
 
 <body>
 	<header>
-		<h1>fran<span style="font-weight: 900">caballero</span></h1>
-		<h4>software developer</h4>
-	</header>
-	<section id="cuerpo">
-		<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
-		<!-- Inicio PHP -->
-		<div style="width: 400px; margin: 150px auto 150px auto; text-align: center; border: 1px solid #dddddd; border-radius: 5px; background: #f5f5f5; padding-bottom: 20px">
-			<h3>Estado de la solicitud</h3>
-			<hr>
-			<?php
-			header('Content-Type: text/html; charset=UTF-8');
-			error_reporting(E_STRICT);
-			require_once('class.phpmailer.php');
-			include('class.smtp.php');
-			$name = utf8_decode($_POST['name']);
-			$email = utf8_decode($_POST['email']);
-			$horas = utf8_decode($_POST['Horas']);
-			$subject = utf8_decode($_POST['subject']);
-			$message = utf8_decode($_POST['message']);
-			$message = 'Mensaje recibido en francaballero.net: <br><br>';
-			$message  = $mensaje . 'Nombre: '.$nombre.'<br>';
-			$message  = $mensaje . 'Email: '.$name.'<br><hr><br>';
-			$message  = $mensaje . utf8_decode($_POST['message']);
-			 $mail = new PHPMailer();
-				$mail->SMTPSecure = 'tls';
-				$mail->Username = "fran.caballero.net@gmail.com";
-				$mail->Password = "oleole32";
-				$mail->AddAddress("fjcc.92@gmail.com);
-				$mail->FromName = $email;
-				$mail->Subject = $subject;
-				$mail->Body = $message;
-				$mail->Host = "smtp.gmail.com";
-				$mail->Port = 587;
-				$mail->IsSMTP();
-				$mail->CharSet = 'UTF-8';
-				$mail->isHTML(true);
-				$mail->SMTPAuth = true;
-				$mail->From = $email;
-				if($mail->Send()) echo 'Sucess<br>You will receive a reply in a few days.';
-				else echo 'Error: ' . $mail->ErrorInfo;
-			?>
-			<br><br><br>
-			<button class="btn btn-success btn-large" onclick="goBack()">Go back</button>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3 col-sm-3 col-xs-12">
+					<h3>fran<span style="font-weight: 600">caballero</span></h3>
+					<p>software developer</p>
+				</div>
+				<div class="col-md-3 col-sm-3 col-xs-0"></div>
+				<div class="col-md-3 col-sm-2 col-xs-0"></div>
+				<div class="col-md-3 col-sm-4 col-xs-12">
+					<ul>
+						<li>22 years old</li>
+						<li>Software Engineering Student</li>
+						<li>University of Cadiz</li>
+						<li>Cadiz, Spain</li>
+					</ul>
+				</div>
+			</div>
 		</div>
-		<!-- Fin PHP -->
-		<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+	</header>
+
+	<div class="blue-bar"></div>
+	<div class="white-bar"></div>
+	
+	<section class="parallax section-contact">
+		<div class="col-md-2 col-sm-2 col-xs-0"></div>
+		<div class="col-md-8 col-sm-8 col-xs-12 contact">
+				<h3>Message Status</h3>
+				<hr>
+				<?php
+				header('Content-Type: text/html; charset=UTF-8');
+				error_reporting(E_STRICT);
+				require_once('class.phpmailer.php');
+				include('class.smtp.php');
+	ini_set('display_errors', '1');
+				$name = utf8_decode($_POST['name']);
+				$email = utf8_decode($_POST['email']);
+				$subject = utf8_decode($_POST['subject']);
+				$message  = $message . 'Name: '.$name.'<br>';
+				$message  = $message . 'E-mail: '.$email.'<br><br><hr>';
+				$message  = $message . '<h4>' . $subject . '</h4>';
+				$message1  = utf8_decode($_POST['message']);
+				$message2 = str_replace("\n",'<br>',$message1);
+				$message = $message . $message2;
+				$message = utf8_encode($message);
+
+				if(isset($_POST['g-recaptcha-response'])){
+				  $captcha=$_POST['g-recaptcha-response'];
+				}
+				if(!$captcha){
+				  echo '<p>Please check the the <strong>captcha</strong> form.</p>';
+				}
+				else {
+					$response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=YOUR SECRET KEY&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']);
+					if($response.success == false)
+					  echo '<p>You are spammer! Get the @$%K out</p>';
+					else {
+						$mail = new PHPMailer();
+						$mail->IsSendmail();
+						$mail->SMTPSecure = 'tls';
+						$mail->Username = 'fran.caballero.net@gmail.com';
+						$mail->Password = 'oleole32';
+						$mail->AddAddress('fjcc.92@gmail.com');
+						$mail->FromName = $email;
+						$mail->Subject = '[francaballero.net] ' . $subject;
+						$mail->Body = $message;
+						
+						if (isset($_FILES['userfile']) &&
+							$_FILES['userfile']['error'] == UPLOAD_ERR_OK) {
+							echo '<p>File has been successfully attached.</p><br>';
+							$mail->AddAttachment($_FILES['userfile']['tmp_name'],
+												 $_FILES['userfile']['name']);
+						}
+						
+						$mail->Host = "smtp.gmail.com";
+						$mail->Port = 587;
+						$mail->IsSMTP();
+						$mail->CharSet = 'UTF-8';
+						$mail->isHTML(true);
+						$mail->SMTPAuth = true;
+						$mail->From = $email;
+						
+						if($mail->Send()) echo '<p>Success.</p><br><p>You will receive a reply in a few days.</p>';
+						else echo 'Error: ' . $mail->ErrorInfo;
+					}
+				}
+				?>
+				<br><br>
+				<button class="btn btn-success btn-large" onclick="goBack()">Go back</button>
+			</div>
+		</div>
 	</section>
-	<br><br><br>
+	<div class="blue-bar"></div>
 	<!--
 	<footer>
 		<br>
